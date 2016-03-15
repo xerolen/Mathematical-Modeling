c3363bff805c9ed72ea93b9bd14913be71e5f396
Momingcoder <a398445075@gmail.com>
2016-03-15 23:39:48 +0800

    Multiple people play

---
+++ Rock-paper-scissors-lizard-Spock/README.md
@@ -0,0 +1,9 @@
+# Rock-paper-scissors-lizard-Spock
+---------
+相信大家都知道剪刀、石头、布这个有趣的游戏（不同的地区有不同的叫法，比如“jinggangchui”）。
+
+Rock-paper-scissors-lizard-Spock 是这个游戏的一个变种，增加了蜥蜴和Spock，看过生活大爆炸的一定对这个不陌生了。
+
+对于游戏的详情，可以查阅WiKipedia：https://en.wikipedia.org/wiki/Rock-paper-scissors
+
+<img src="Pierre_ciseaux_feuille_lézard_spock_aligned.svg" width = "500" />
--- Rock-paper-scissors-lizard-Spock/Rock-paper-scissors-lizard-Spock.py
+++ Rock-paper-scissors-lizard-Spock/Rock-paper-scissors-lizard-Spock.py
@@ -17,29 +17,67 @@
 
 import random
 
-def rpsls(x, y):
+def rpsls_with_computer(player):
+    '''
+    When player wants to play with computer, then generates a random tag.
+    '''
+    x = choice[player]
+    y = random.randint(0, 4)
     tmp = (x - y) % 4;
     if tmp <= 2 and tmp > 0:
-        print 'Player (' + str(Sequence[x]) + ') wins computer (' + str(Sequence[y]) + ').'
+        print 'Player (' + str(sequence[x]) + ') wins computer (' + str(sequence[y]) + ').'
     elif tmp == 0:
-        print 'Player (' + str(Sequence[x]) + ') is equal to computer (' + str(Sequence[y]) + ').'
+        print 'Player (' + str(sequence[x]) + ') is equal to computer'
     else:
-        print 'Computer (' + str(Sequence[y]) + ') wins player (' + str(Sequence[x]) + ').'
+        print 'Computer (' + str(sequence[y]) + ') wins player (' + str(sequence[x]) + ').'
 
 
-Choice = {'rock':0, 'Spock':1, 'paper':2, 'lizard':3, 'scissors':4}
-Sequence = {0:'rock', 1:'Spock', 2:'paper', 3:'lizard', 4:'scissors'}
+def rpsls(choice_list):
+    '''
+    When players play with each other, outputs the winner's index if exits.
+    '''
+    flag = False
+    for i in range(5):
+        if choice_list[choice_key[i]] == choice_list[choice_key[(i + 1) % 5]] == '':
+            if not choice_list[choice_key[(i - 1) % 5]] == '':
+                flag = True
+                print 'Player' + ' Player'.join(choice_list[choice_key[(i - 1) % 5]]) + ' win the game.'
+                print '(index from 0.)'
+                return
 
-circle = 5
-while circle > 0:
-    circle = circle - 1
-    player = input()
-    if player not in Choice.keys():
-        print 'Wrong input!'
-        continue
+    if flag == False:
+        print 'No one wins.'
+    return
 
-    player_seq = Choice[player]
 
-    computer_seq = random.randint(0, 4)
+choice = {'rock':0, 'Spock':1, 'paper':2, 'lizard':3, 'scissors':4}
+choice_key = choice.keys()
+sequence = {0:'rock', 1:'Spock', 2:'paper', 3:'lizard', 4:'scissors'}
+choice_list = {'rock':'', 'Spock':'', 'paper':'', 'lizard':'', 'scissors':''}
 
-    rpsls(player_seq, computer_seq)
+
+player_num = int(input('Please input the number of players: '))
+
+if player_num < 1:
+    # get a wrong input
+    print 'Wrong input'
+
+elif player_num == 1:
+    # play with computer
+    player = input('Please input your choice: ')
+    while player not in choice_key:
+        print 'Wrong input! Try again!'
+        player = input('Please input your choice: ')
+
+    rpsls_with_computer(player)
+
+else:
+    # play with people
+    for i in range(player_num):
+        player = input('Please input your choice: ')
+        while player not in choice_key:
+            print 'Wrong input! Try again!'
+            player = input('Please input your choice: ')
+        choice_list[player] = choice_list[player] + str(i)
+
+    rpsls(choice_list)
