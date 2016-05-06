5bca5a9c33632b9de0bedc0c4cfd8263bbd68a61
Momingcoder <a398445075@gmail.com>
2016-05-06 23:57:02 +0800

    KNN

--- KNN/KNN.m
+++ KNN/KNN.m
@@ -6,16 +6,25 @@ function [result] = KNN(k, train, test)
 
 [m, ~] = size(test);
 num = size(train, 1);
-result = zeros(m, 1);
+if k > num % k should not bigger than num
+    k = num;
+end
 dist = zeros(m, num);
+index = zeros(m, k);
 
 for i = 1 : m
     for j = 1 : num
+        % calculate the distance (Euclidean distance)
         dist(i, j) = sqrt(sum( ( test(i, :) - train(j, 2 : end) ).^2 ));
     end
 end
 
+[~, pos] = sort(dist, 2);
 for i = 1 : m
-    [~, index] = min(dist(i, :));
-    result(i) = train(index, 1);
+    for j = 1 : k
+        index(i, j) = train(pos(i, j), 1);
+    end
 end
+
+% mode() will return the min one if some numbers' frequencies are the same
+result = mode(index')'; 
