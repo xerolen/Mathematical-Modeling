3c8f4499e764dc9426044fbe7184a6510edbd8fc
Momingcoder <a398445075@gmail.com>
2016-05-06 23:39:25 +0800

    git ignore

--- KNN/KNN.m
+++ KNN/KNN.m
@@ -1,4 +1,21 @@
+function [result] = KNN(k, train, test)
 % KNN
 % @Author: Moming
 % 2016-05-06
+% the first colume of train is the label
 
+[m, ~] = size(test);
+num = size(train, 1);
+result = zeros(m, 1);
+dist = zeros(m, num);
+
+for i = 1 : m
+    for j = 1 : num
+        dist(i, j) = sqrt(sum( ( test(i, :) - train(j, 2 : end) ).^2 ));
+    end
+end
+
+for i = 1 : m
+    [~, index] = min(dist(i, :));
+    result(i) = train(index, 1);
+end
